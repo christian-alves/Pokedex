05ef8c5d15b16d654882105f3f702282db20153d
Christian Ricardo Alves <57810657+chronosdeal@users.noreply.github.com>
2019-12-13 18:20:48 -0300

    PokeCard aparece apenas on submit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ class App extends React.Component{
     this.getPokemon = this.getPokemon.bind(this);
 
     this.state = {
-      pokemon: ''
+      pokemon: '',
+      types: [],
+      isSearched:false,
     };
   }
 
@@ -22,20 +24,42 @@ class App extends React.Component{
       );
       const pokemon = await res.json();
       this.setState({
-        pokemon: pokemon
+        pokemon: pokemon,
+        isSearched:true
       })
   }
 
   handleChange(e){
    this.setState({
-     pokemon: e.target.value
+     pokemon: e.target.value,
+     isSearched: false
     });
   }
 
+  Capitalize(str){
+    return str.charAt(0).toUpperCase() + str.slice(1);
+    }
 
   render(){
+    let types = this.state.pokemon.types.map((item, key) =>
+    <li key={item.id}>{item.name}</li>
+    );
     console.log(this.state.pokemon);
+    const isSearched = this.state.isSearched
+    let Pokecard;
+    if(isSearched){
+      Pokecard = 
+      <PokeCard
+      iconsrc={this.state.pokemon.sprites ? this.state.pokemon.sprites.front_default: ''}
+      name={this.Capitalize(this.state.pokemon.name)}
+      height={this.state.pokemon.height}
+      weight={this.state.pokemon.weight}
+      elements={types}
+      />;
 
+    }else{
+      Pokecard = null;
+    }
   return (
     <div className="App">
       <Header/>
@@ -45,12 +69,7 @@ class App extends React.Component{
             onChange={this.handleChange}/>        
           </form>
         </div>
-      <PokeCard 
-      iconsrc={this.state.pokemon.sprites? this.state.pokemon.sprites.front_default: ''}
-      name={this.state.pokemon.name}
-      height={this.state.pokemon.height}
-      weight={this.state.pokemon.weight}
-      />
+      {Pokecard}
     </div>
   );
 }
--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -7,9 +7,12 @@ export default function PokeCard(props) {
     <div className="container">
     <div className="pokecard">
       <img src={props.iconsrc} alt="icon"/>
-      <h1>Nome :{props.name}</h1>
-      <h2>Altura :{props.height}</h2>
-      <h2>Peso :{props.weight}</h2>
+      <h1>Nome: {props.name}</h1>
+      <h2>Altura: {props.height}m</h2>
+      <h2>Peso: {props.weight}kg</h2>
+      <ul>
+        {props.elements}
+      </ul>
     </div>
     </div>
   );
